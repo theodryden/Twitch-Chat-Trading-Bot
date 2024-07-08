e523bef5d4c2afad1bc33f15992df3f3dcd4c83f
Theodryden <theodryden@yahoo.com>
2024-07-08 22:18:10 +0100

    Updated Font Sizes in table and guide.

--- socialstocktraderui/components/TradingGuide.js
+++ socialstocktraderui/components/TradingGuide.js
@@ -6,35 +6,27 @@ const TradingGuide = () => {
   return (
 
     <div className={styles.TradingGuide}>
-      <h2>Trading Guide</h2>
+      <h1>Trading Guide</h1>
 
-      <p><strong>Summary</strong></p>
+      <h2><strong>Summary</strong></h2>
       <p>Gone are the days when you had a sixth-sense that a company's share price was about to sky rocket but you had no-one to share your investing idea with.</p>
       <p>YAFA is your own community managed EQUITY portfolio.</p>
 
-      <p><strong>Do I need anything to participate?</strong></p>
+      <h3><strong>Do I need anything to participate?</strong></h3>
       <p>You will need a Twitch account to send messages in the Twitch chat</p>
 
-      <p><strong>How do I make a Twitch account?</strong></p>
+      <h3><strong>How do I make a Twitch account?</strong></h3>
       <p>Follow this guide</p>
 
-      <p><strong>How do I place a trade?</strong></p>
+      <h3><strong>How do I place a trade?</strong></h3>
       <p><code>!trade Direction Stock-Symbol Quantity</code></p>
       <p><code>Example: !trade buy MSFT 100</code></p>
       <p><code>Example: !trade sell MSFT 100</code></p>
 
-      <p><strong>How do I know if the trade was successful?</strong></p>
+      <h3><strong>How do I know if the trade was successful?</strong></h3>
       <p>Check the pie chart on the homepage for your trade!</p>
    
-      <p><strong>Which stocks should I choose?</strong></p>
-      <ul>
-        <li>1. Pick a stock you think will go up in value</li>
-        <li>2. Place your trade through the Twitch Chat</li>
-        <li>3. Let the community know your thoughts and rationale.</li>
-        <li>4. Enjoy the ride upwards or sell the stock </li>
-      </ul>
-
-      <p><strong>Do you have examples of any stock symbols I could use?</strong></p>
+      <h3><strong>Do you have examples of any stock symbols I could use?</strong></h3>
       <p>It is encouraged you do your own research however, see below for a list of well known stocks and their corresponding symbols</p>
 
     </div>
