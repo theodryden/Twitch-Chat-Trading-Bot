a1301c07fedf0737ca39402c2ec5fcea1169a96d
Theodryden <theodryden@yahoo.com>
2024-07-09 13:21:08 +0100

    Updated Account History with order direction/side

--- socialstocktraderui/components/OrderHistory.js
+++ socialstocktraderui/components/OrderHistory.js
@@ -1,6 +1,7 @@
 // src/components/OrderHistory.js
 import React from 'react';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import styles from '../components/OrderHistory.module.css'
 
 // Define the prop types for the orders
 const OrderHistory = ({ orders }) => {
@@ -9,14 +10,14 @@ const OrderHistory = ({ orders }) => {
   }
 
   return (
-    <TableContainer component={Paper}>
+    <TableContainer component={Paper} className={styles.OrderHistory}>
       <Table>
         <TableHead>
           <TableRow>
             <TableCell><strong>Order ID</strong></TableCell>
             <TableCell><strong>Symbol</strong></TableCell>
             <TableCell><strong>Quantity</strong></TableCell>
-            <TableCell><strong>Filled Quantity</strong></TableCell>
+            <TableCell><strong>Direction</strong></TableCell>
             <TableCell><strong>Average Filled Price</strong></TableCell>
             <TableCell><strong>Status</strong></TableCell>
           </TableRow>
@@ -27,7 +28,7 @@ const OrderHistory = ({ orders }) => {
               <TableCell>{order.id}</TableCell>
               <TableCell>{order.symbol}</TableCell>
               <TableCell>{order.qty}</TableCell>
-              <TableCell>{order.filled_qty}</TableCell>
+              <TableCell>{order.side}</TableCell>
               <TableCell>${order.filled_avg_price}</TableCell>
               <TableCell>{order.status}</TableCell>
             </TableRow>
